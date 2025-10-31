3ceab30c0949a36dfd72f19f735b15cc82f1c13e
TanishRS <tanishsuvarna07@gmail.com>
2025-10-31 11:03:58 +0530

    second message

--- range.py
+++ range.py
@@ -3,9 +3,13 @@
 # print(range(0,5))
 # print(list(range(0,5,2)))
 
+# for x in range(0,5):
+#     print(x)
+
 nums="prasad"
 for x in reversed(nums):
     print(x)
 
-for x in range(0,5):
-    print(x)
+[print(x in nums)]
+print(len(nums))
+
