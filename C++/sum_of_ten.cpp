e239ec2ab1b2e154ba57d20a090f9607ecdf422c
TanishRS <tanishsuvarna07@gmail.com>
2026-01-07 10:43:53 +0530

    New_C++_Files

---
+++ C++/sum_of_ten.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    int sum = 0;
+    while (i<=10){
+        sum += i;
+        i ++;
+    }
+    cout << "Sum: " << sum;
+
+    return 0;
+
+}
