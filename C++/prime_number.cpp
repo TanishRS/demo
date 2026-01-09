e9c5848a683cb934d2bf1d1b250897cc6a5f0af5
TanishRS <tanishsuvarna07@gmail.com>
2026-01-09 13:17:45 +0530

    New_C++_Files

---
+++ C++/prime_number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j;
+
+    for (i = 2; i <= 100; i++) {
+        for (j = 2; j <= i; j++) {
+            if (i % j == 0)
+                break;
+        }
+
+        if (j == i) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
