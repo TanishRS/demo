c6f0854e4b186ddb5cf218cbf5ba9a64dfd3cde2
TanishRS <tanishsuvarna07@gmail.com>
2026-01-22 15:37:25 +0530

    New files

---
+++ JavaScript/array_js.js
@@ -0,0 +1,13 @@
+let subjects = ["HTML", "CSS", "JavaScript", "Python", "Java"];
+let marks = [85, 90, 95, 80, 70];
+let students = ["Alice", 26, "Mumbai"];
+console.log("Subjects:", subjects);
+console.log("Marks:", marks);
+console.log("Student Details:", students);
+
+let total = 0;
+for (let i = 0; i < marks.length; i++) {
+    total += marks[i];
+}
+console.log("Total Marks:", total);
+    
