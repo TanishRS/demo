6be27781189393b5670484c89f2b7fa86626549f
TanishRS <tanishsuvarna07@gmail.com>
2026-01-28 13:32:54 +0530

    New JS Files

---
+++ JavaScript/email.js
@@ -0,0 +1,12 @@
+let details = {
+    name: "Rohan",
+    id: 101,
+    email: "rohan@example.com"
+};
+console.log(details)
+
+if ("age" in details) {
+    console.log("Age property exists in details object");
+} else {
+    console.log("Age property does not exist in details object");
+};
---
+++ JavaScript/object.js
@@ -0,0 +1,13 @@
+let person = {
+    name: "Asha",
+    age: 25,
+    city: "Nashik"
+};
+console.log(person)
+
+let book = {
+    title: "The Alchemist",
+    Author: "Paulo Coelho",
+    year: 1988
+};
+console.log(book)
